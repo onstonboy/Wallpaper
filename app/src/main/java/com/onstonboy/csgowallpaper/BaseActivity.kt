ce04e1544a45050ad5d27ff9ac9a5459649e2a7c
Lê Văn Chương <vanchuong119@gmail.com>
2019-08-24 00:05:53 +0700

    Load resource from server PART 1

---
+++ app/src/main/java/com/onstonboy/csgowallpaper/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.onstonboy.csgowallpaper
+
+import android.util.Log
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
+
+abstract class BaseActivity : AppCompatActivity() {
+    override fun onResume() {
+        super.onResume()
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+    }
+
+    fun logError(tag: String, message: String, exception: Exception) {
+        Log.e(tag, message, exception)
+    }
+}
---
+++ app/src/main/java/com/onstonboy/csgowallpaper/scene/main/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.onstonboy.csgowallpaper.scene.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.onstonboy.csgowallpaper.BaseActivity
+import com.onstonboy.csgowallpaper.R
+import com.onstonboy.csgowallpaper.scene.dota2.Dota2PackageActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_home -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_tools -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    fun onDota2ViewClick(view: View) {
+        startActivity(Intent(this, Dota2PackageActivity::class.java))
+    }
+}
