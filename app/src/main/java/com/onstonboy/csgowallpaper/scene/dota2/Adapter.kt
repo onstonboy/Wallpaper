9bf35b5fab12c1325684825688f8e3768a65d01c
Lê Văn Chương <vanchuong119@gmail.com>
2019-08-24 22:28:42 +0700

    Load resource from server PART 2

--- app/src/main/java/com/onstonboy/csgowallpaper/scene/dota2/Adapter.kt
+++ app/src/main/java/com/onstonboy/csgowallpaper/scene/dota2/Adapter.kt
@@ -1,29 +1,40 @@
 package com.onstonboy.csgowallpaper.scene.dota2
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.onstonboy.csgowallpaper.R
+import com.onstonboy.csgowallpaper.base.LoadMoreRecyclerViewAdapter
 import com.onstonboy.csgowallpaper.extension.loadUrlWithPlaceHolder
 import kotlinx.android.synthetic.main.item_grid_image.view.*
 
-class Adapter : RecyclerView.Adapter<Adapter.ItemViewHolder>() {
+class Adapter(context: Context) : LoadMoreRecyclerViewAdapter<String>(context) {
 
-    private var mDataList = ArrayList<String>()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_grid_image, parent, false)
-        return ItemViewHolder(view)
+        if (ITEM_IMAGE == viewType) {
+            return ItemViewHolder(view)
+        }
+        return super.onCreateViewHolder(parent, viewType)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder.itemViewType == ITEM_IMAGE) {
+            (holder as ItemViewHolder).bind(mDataList[position])
+        } else {
+            super.onBindViewHolder(holder, position)
+        }
     }
 
     override fun getItemCount(): Int {
         return mDataList.size
     }
 
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.bind(mDataList[position])
+    override fun getCustomItemViewType(position: Int): Int {
+        return ITEM_IMAGE
     }
 
     fun addData(url: String) {
@@ -31,14 +42,13 @@ class Adapter : RecyclerView.Adapter<Adapter.ItemViewHolder>() {
         notifyItemInserted(mDataList.size)
     }
 
-    fun updateData(urls: List<String>) {
-        mDataList.addAll(urls)
-        notifyDataSetChanged()
-    }
-
     class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(url: String) {
             itemView.imageView.loadUrlWithPlaceHolder(url, R.drawable.ic_photo_placeholder)
         }
+    }
+
+    companion object {
+        private const val ITEM_IMAGE = 0
     }
 }
